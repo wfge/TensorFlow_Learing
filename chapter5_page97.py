07f2cdd3a944c3770c84e2f63314ca152099b67c
SU <1131852344@qq.com>
2018-11-05 22:21:47 +0800

    2018/11/5 in SU

--- chapter5_page97.py
+++ chapter5_page97.py
@@ -28,10 +28,36 @@ def train(mnist):
     weights1=tf.Variable(tf.truncated_normal([INPUT_NODE,LAYER1_NODE],stddev=0.1))
     biases1=tf.Variable(tf.constant(0.1,shape=[LAYER1_NODE]))
 
+    weights2=tf.Variable(tf.truncated_normal([LAYER1_NODE,OUTPUT_NODE],stddev=0.1))
+    biases2=tf.Variable(tf.constant(0.1,shape=[OUTPUT_NODE]))
+
+    y=inference(x,None,weights1,biases1,weights2,biases2)
+    global_step=tf.Variable(0,trainable=False)
+
+    variable_averges=tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
+    variable_averges_op=variable_averges.apply(tf.trainable_variables())
+
+    average_y=inference(x,variable_averges,weights1,biases1,weights2,biases2)
+
+    cross_entroy=tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,labels=tf.arg_max(y_,1))
+    cross_entroy_mean=tf.reduce_mean(cross_entroy)
+
+    regularizer=tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+    regularization=regularizer(weights1)+regularizer(weights2)
+
+    loss=cross_entroy_mean+regularization
+
+    learning_rate=tf.train.exponential_decay(
+        LEARNING_RATE_BASE,
+        global_step,
+        mnist.train.num_examples,
+        LEARNING_RATE_DECAY
+    )
+    train_step=tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
+
 def main(argv=None):
     #mnist=input_data.read_data_sets("/tmp/data",one_hot=True)
     mnist = input_data.read_data_sets("/path/to/MNIST_data", one_hot=True)
     print(mnist.train.num_examples)
 
 
-main()
