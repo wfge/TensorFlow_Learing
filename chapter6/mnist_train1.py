d3b29078d25e0ee1d24be75271a31f5fc34e7963
SU <1131852344@qq.com>
2018-11-21 22:29:36 +0800

    2018/11/8 in SU

---
+++ chapter6/mnist_train1.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import chapter6.mnist_inference as ch6mi
+import os
+import numpy as np
+
+BATCH_SIZE = 100
+LEARNING_RATE_BASE = 0.8
+LEARNING_RATE_DECAY = 0.99
+REGULARIZATION_RATE = 0.0001
+TRAINING_STEPS = 30000
+MOVING_AVERAGE_DECAY = 0.99
+
+MODEL_SAVE_PATH="./model/"
+MODEL_NAME="chapter6_model1.ckpt"
+
+
+def train(mnist):
+    x = tf.placeholder(tf.float32, [BATCH_SIZE, ch6mi.IMAGE_SIZE, ch6mi.IMAGE_SIZE, ch6mi.NUM_CHANNELS], name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, ch6mi.OUTPUT_NODE], name='y-input')
+
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+    y = ch6mi.inference(x, 1,regularizer)
+    global_step = tf.Variable(0, trainable=False)
+
+
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+    learning_rate = tf.train.exponential_decay(
+        LEARNING_RATE_BASE,
+        global_step,
+        mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY,
+        staircase=True)
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    with tf.control_dependencies([train_step, variables_averages_op]):
+        train_op = tf.no_op(name='train')
+
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+
+        for i in range(TRAINING_STEPS):
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            reshaped_xs = np.reshape(xs, (BATCH_SIZE, ch6mi.IMAGE_SIZE, ch6mi.IMAGE_SIZE, ch6mi.NUM_CHANNELS))
+            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: reshaped_xs, y_: ys})
+            if i % 1000 == 0:
+                print("After %d training step(s), loss on training batch is %g." % (step, loss_value))
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("/path/to/mnist_data", one_hot=True)
+    train(mnist)
+
+if __name__ == '__main__':
+    tf.app.run()
+
