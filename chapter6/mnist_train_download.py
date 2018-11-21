64b3248eb7a1521b5ce4fe3ea7a43fefe6a6f636
SU <1131852344@qq.com>
2018-11-21 22:48:08 +0800

    2018/11/21 in SU

--- chapter6/mnist_train_download.py
+++ chapter6/mnist_train_download.py
@@ -1,29 +1,47 @@
+# coding: utf-8
+
+# In[1]:
+
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import chapter6.mnist_inference1 as ch6mi
+import chapter6.mnist_inference1 as LeNet5_infernece
 import os
 import numpy as np
 
+# #### 1. 定义神经网络相关的参数
+
+# In[2]:
+
+
 BATCH_SIZE = 100
-LEARNING_RATE_BASE = 0.8
+LEARNING_RATE_BASE = 0.01
 LEARNING_RATE_DECAY = 0.99
 REGULARIZATION_RATE = 0.0001
-TRAINING_STEPS = 30000
+TRAINING_STEPS = 6000
 MOVING_AVERAGE_DECAY = 0.99
 
-MODEL_SAVE_PATH="./model/"
-MODEL_NAME="chapter6_model1.ckpt"
+
+# #### 2. 定义训练过程
+
+# In[3]:
 
 
 def train(mnist):
-    x = tf.placeholder(tf.float32, [BATCH_SIZE, ch6mi.IMAGE_SIZE, ch6mi.IMAGE_SIZE, ch6mi.NUM_CHANNELS], name='x-input')
-    y_ = tf.placeholder(tf.float32, [None, ch6mi.OUTPUT_NODE], name='y-input')
+    # 定义输出为4维矩阵的placeholder
+    x = tf.placeholder(tf.float32, [
+        BATCH_SIZE,
+        LeNet5_infernece.IMAGE_SIZE,
+        LeNet5_infernece.IMAGE_SIZE,
+        LeNet5_infernece.NUM_CHANNELS],
+                       name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, LeNet5_infernece.OUTPUT_NODE], name='y-input')
 
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
-    y = ch6mi.inference(x, 0,regularizer)
+    y = LeNet5_infernece.inference(x, False, regularizer)
     global_step = tf.Variable(0, trainable=False)
 
-
+    # 定义损失函数、学习率、滑动平均操作以及训练过程。
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
     variables_averages_op = variable_averages.apply(tf.trainable_variables())
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
@@ -34,28 +52,39 @@ def train(mnist):
         global_step,
         mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY,
         staircase=True)
+
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
     with tf.control_dependencies([train_step, variables_averages_op]):
         train_op = tf.no_op(name='train')
 
-
+    # 初始化TensorFlow持久化类。
     saver = tf.train.Saver()
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
-
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-            reshaped_xs = np.reshape(xs, (BATCH_SIZE, ch6mi.IMAGE_SIZE, ch6mi.IMAGE_SIZE, ch6mi.NUM_CHANNELS))
+
+            reshaped_xs = np.reshape(xs, (
+                BATCH_SIZE,
+                LeNet5_infernece.IMAGE_SIZE,
+                LeNet5_infernece.IMAGE_SIZE,
+                LeNet5_infernece.NUM_CHANNELS))
             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: reshaped_xs, y_: ys})
+
             if i % 1000 == 0:
                 print("After %d training step(s), loss on training batch is %g." % (step, loss_value))
-                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+
+# #### 3. 主程序入口
+
+# In[4]:
 
 
 def main(argv=None):
     mnist = input_data.read_data_sets("/path/to/mnist_data", one_hot=True)
     train(mnist)
 
+
 if __name__ == '__main__':
-    tf.app.run()
+    main()
 
